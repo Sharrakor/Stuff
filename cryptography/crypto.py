c71b7f27875a3460f4bdff3a410f252f673d37eb
Nathan Gil <darthmavoc@gmail.com>
2013-08-20 23:28:44 +1000

    Adds the cryptography shit I'm screwing around with

---
+++ cryptography/crypto.py
@@ -0,0 +1,21 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Babby\'s first encryptor.')
+parser.add_argument('infile', type=argparse.FileType('r'), default=sys.stdout, help='the name of the file to be encrypted')
+parser.add_argument('key', help='a key to encrypt the file with')
+parser.add_argument('-o','--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='the name of a file to output the encrypted information to')
+
+args = parser.parse_args()
+
+def main():	
+	for line in args.infile:
+		print(line, end='')
+		## Todo: The actual cryptography part of the cryptography thing, just as soon as I've figured out how '>_>
+
+	command = input("Exit? [Y/N]: ").lower()
+	while command != "y":
+		command = input("Exit? [Y/N]: ").lower()
+
+
+main()
